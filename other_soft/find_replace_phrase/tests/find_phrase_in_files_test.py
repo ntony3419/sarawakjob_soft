8e441c4308afbbb637a7bbd51df9479a8d1d7466
tony <ntony3419@gmail.com>
2022-06-28 14:18:43 +0700

    update

--- other_soft/find_replace_phrase/tests/find_phrase_in_files_test.py
+++ other_soft/find_replace_phrase/tests/find_phrase_in_files_test.py
@@ -1,11 +1,11 @@
 
 import sys
-sys.path.append(r'D:\repos\sarawakjob_soft') #temporary add the path that contain other_soft package
+sys.path.append(r'D:\repos') #temporary add the path that contain other_soft package
 
 
-import other_soft.find_replace_phrase.source.searchkeyword as search_funs
+import sarawakjob_soft.other_soft.find_replace_phrase.source.searchkeyword as search_funs
 
 search_funs.find_phrase_in_folder(r"D:\SabahJobs.com @ 24 Mar 2022\SabahJobs.com @ 24 Mar 2022\wp-content\themes\sjobs",
-                                 'You are a bot')
+                                 'apllication/ld+json')
 #candidatetable dataTable no-footer 
 #button-applicants
